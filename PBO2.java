cd1d3616c10a4ac1f89a495440ed3c370f18b406
AhmadSubhan <33022323+Ahmsubhan@users.noreply.github.com>
2018-01-20 23:51:44 +0800

    Add files via upload

---
+++ PBO2.java
@@ -0,0 +1,13 @@
+
+package pbo2;
+public class PBO2 {
+    public static void main(String[]args){
+        myFrame m = new myFrame();
+        
+        m.pesan = "Hallo..";
+        m.bilBulat = 5;
+        m.bilDesimal = 3.14;
+        
+        m.cetakAttribute();
+    }
+}
